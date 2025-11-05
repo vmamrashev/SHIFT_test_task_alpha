11d1bc82987059c61613b0f1203f5ebd87d17737
Super Thaddeus <supertha@student.21-school.ru>
2025-11-05 13:37:45 +0700

    Main class changed to test Configuration class

--- src/main/java/testtask/Main.java
+++ src/main/java/testtask/Main.java
@@ -1,14 +1,19 @@
 package testtask;
+
 import java.util.ArrayList;
 import java.util.List;
 import testtask.fileprocessor.FilesReader;
+import testtask.cliparser.Configuration;
 public class Main {
 
     public static void main(String[] args) {
         List<String> files = new ArrayList<>();
         files.add("./1.txt");
         files.add("./2.txt");
-        FilesReader fr = new FilesReader(files);
+
+        //FilesReader fr = new FilesReader(files);
+        Configuration c = new Configuration(true, true, "./", "pref_", files);
+        FilesReader fr = new FilesReader(c.getFiles());
 
         String str = fr.getNextString();
         while (str != null) {
