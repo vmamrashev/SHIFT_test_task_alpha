b644b01ae7c4dcece537c0a5d5cdd585ea6cd312
Super Thaddeus <supertha@student.21-school.ru>
2025-11-11 18:03:19 +0700

    Draft implementations of Int-, Double-, String- implementations of StatsCollector interface added

---
+++ src/main/java/testtask/statscollector/IntStatsCollector.java
@@ -0,0 +1,25 @@
+package testtask.statscollector;
+
+import testtask.sorter.SortResult;
+
+public class IntStatsCollector implements StatsCollector{
+
+    private  int maxLength = 0;
+    private  int minLength = Integer.MAX_VALUE;
+    private  int count;
+    private  boolean needFullStats;
+    private int currentStringLength;
+
+    public void addValue(SortResult value){
+        count++;
+        if (needFullStats) {
+            currentStringLength = String.valueOf(value.getStringValue()).length();
+            if (currentStringLength > maxLength) maxLength = currentStringLength;
+            if (currentStringLength < minLength) minLength = currentStringLength;
+        }
+    }
+
+    public String getStats(){
+        return "String stats";
+    }
+}
---
+++ src/main/java/testtask/statscollector/StringStatsCollector.java
@@ -0,0 +1,24 @@
+package testtask.statscollector;
+
+import testtask.sorter.SortResult;
+
+public class StringStatsCollector implements StatsCollector{
+    private  int maxLength = 0;
+    private  int minLength = Integer.MAX_VALUE;
+    private  int count;
+    private  boolean needFullStats;
+    private int currentStringLength;
+
+    public void addValue(SortResult value){
+        count++;
+        if (needFullStats) {
+            currentStringLength = String.valueOf(value.getStringValue()).length();
+            if (currentStringLength > maxLength) maxLength = currentStringLength;
+            if (currentStringLength < minLength) minLength = currentStringLength;
+        }
+    }
+
+    public String getStats(){
+        return "String stats";
+    }
+}
