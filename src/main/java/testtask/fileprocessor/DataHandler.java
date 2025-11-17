2b88c99d74512f9fc1139b1c5c44990d2dac3c15
Super Thaddeus <supertha@student.21-school.ru>
2025-11-18 00:15:33 +0700

    Added StatsCollectorFactory as a field to DataHandler class. Added stats collection via factory to handle method

--- src/main/java/testtask/fileprocessor/DataHandler.java
+++ src/main/java/testtask/fileprocessor/DataHandler.java
@@ -1,6 +1,7 @@
 package testtask.fileprocessor;
 import testtask.sorter.SortResult;
 import testtask.cliparser.Configuration;
+import testtask.statscollector.StatsCollectorFactory;
 
 public class DataHandler {
     boolean isIntFirstOccurrence = true;
@@ -10,10 +11,12 @@ public class DataHandler {
     boolean isFullStats = false;
     private String outputPath;
     private String outputFilenamesPrefix;
+    private StatsCollectorFactory scf;
     Configuration cfg;
 
-    public DataHandler(Configuration cfg){
+    public DataHandler(Configuration cfg, StatsCollectorFactory scf){
         this.cfg = cfg;
+        this.scf = scf;
         this.isFullStats = cfg.isFullStats();
         this.isAppend = cfg.isAppend();
         this.outputPath = cfg.getOutputPath();
@@ -21,6 +24,6 @@ public class DataHandler {
     }
 
     public void handle(SortResult value){
-
+        scf.getCollector(value.getType()).addValue(value);
     }
 }
